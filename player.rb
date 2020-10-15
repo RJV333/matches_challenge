92bdb83788b56449b24617af8c4a44b3519fb24f
Ryan Vasios <ryan.vasios@annkissam.com>
2020-10-15 15:32:15 -0400

    Update Player class

--- player.rb
+++ player.rb
@@ -8,7 +8,9 @@ class Player
 
   # heaps: 7 element array of integers with values 0 - 6, representing board
 
-  # returns two integers: [column_index, disc_removal_amount]
+  # returns two integer array: [column_index, disc_removal_amount]
+  # column_index: (0-6) column to remove discs from
+  # disc_removal_amount: positive integer 1-6 for number of discs to remove
   def play_round(heaps)
     fail 'NOT IMPLEMENTED'
   end
